e2dad542ead17f63941c071d62221305d887cb04
artur.bulakaev <amber@ua.fm>
2016-10-18 19:51:19 +0300

    solution for restarting due missing new port;

--- TelergramEALLOBot/Program.cs
+++ TelergramEALLOBot/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -47,19 +48,28 @@ namespace TelergramEALLOBot
 			// для heroku
 			int port = 1234;
 
-			try
-			{
-				port = Int32.Parse( System.Environment.GetEnvironmentVariable( "PORT" ) );
-			}
-			catch(Exception ex)
-			{
-				Console.WriteLine( ex.ToString() );
-			}
+			var threadServer = new Thread(
+			()=>
+				{
+					while ( true )
+					{
+						try
+						{
+							port = Int32.Parse( System.Environment.GetEnvironmentVariable( "PORT" ) );
+						}
+						catch ( Exception )
+						{
+							continue;
+						}
 
-			TcpListener server = new TcpListener( IPAddress.Parse( "127.0.0.1" ), port );
-			Console.WriteLine( "server before lis" );
-			server.Start();
-			Console.WriteLine( "server after lis" );
+						TcpListener server = new TcpListener( IPAddress.Parse( "127.0.0.1" ), port );
+						server.Start();
+						Thread.Sleep( 30000 );
+						server.Stop();
+					}
+				} 
+			);
+			threadServer.Start();
 
 			while ( true )
 			{
@@ -69,7 +79,7 @@ namespace TelergramEALLOBot
 			}
 
 			Bot.StopReceiving();
-			server.Stop();
+			threadServer.Abort();
 		}
 
 		private static async void BotOnMessageReceived( object sender, MessageEventArgs messageEventArgs )
